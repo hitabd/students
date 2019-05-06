1c08743f5c938a75d275aefc01f9ec3a5982852f
AhamedZaman <ahamed.zaman.adnan@gmail.com>
2019-05-06 10:41:53 +0600

    hmmm

--- app/Student.php
+++ app/Student.php
@@ -10,7 +10,7 @@ class Student extends Model
 {
     protected $table = 'students';
 
-    protected $fillable = [];
+    protected $fillable = ['residensial', 'image', 'name_bn', 'name_en', 'f_name_bn', 'f_name_en', 'f_occupation', 'f_income', 'm_name_bn', 'm_name_en', 'm_occupation', 'm_income', 'p_village', 'p_house', 'p_post', 'p_thana', 'p_district', 'per_village', 'per_house', 'per_post', 'per_thana', 'per_district', 'dob', 'b_group', 'height'];
 
     public function madrashas(){
         return $this->hasMany(preMadrasha::class, 'student_id');
